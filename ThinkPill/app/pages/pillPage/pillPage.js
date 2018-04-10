d5b5eb14fdc2ccca90780d634f02f1126afbd383
Leilanie Morales <leilanie.morales@upr.edu>
2018-04-10 11:23:54 -0400

    pill

--- ThinkPill/app/pages/pillPage/pillPage.js
+++ ThinkPill/app/pages/pillPage/pillPage.js
@@ -44,3 +44,15 @@ function onNavigatingTo(args) {
 }
 
 exports.onNavigatingTo = onNavigatingTo;
+
+var view = require("ui/core/view");
+var drawer;
+
+exports.pageLoaded = function(args) {
+    var page = args.object;
+    drawer = view.getViewById(page, "sideDrawer");
+};
+
+exports.toggleDrawer = function() {
+    drawer.toggleDrawerState();
+};
