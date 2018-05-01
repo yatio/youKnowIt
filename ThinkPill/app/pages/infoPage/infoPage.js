708d4f03572c960965d08f2fa0c931e11d1efd22
Leilanie <leilanie.morales@upr.edu>
2018-05-01 15:47:36 -0400

    changes
    cree Pagina de Cabeza(dolor), los botones del listview tienen link a esa pagina

--- ThinkPill/app/pages/infoPage/infoPage.js
+++ ThinkPill/app/pages/infoPage/infoPage.js
@@ -1,49 +1,76 @@
-function onNavigatingTo(args) {
-    var page = args.object;
-}
-exports.onNavigatingTo = onNavigatingTo;
+// function onNavigatingTo(args) {
+//     var page = args.object;
+// }
+// exports.onNavigatingTo = onNavigatingTo;
 
-function onNavigatingTo(args) {
-    var page = args.object;
+// function onNavigatingTo(args) {
+//     var page = args.object;
     
-    var gotData=page.navigationContext;
-    // console.log(gotData.param1);
-    // console.log(gotData.param2);
+//     var gotData=page.navigationContext;
+//     // console.log(gotData.param1);
+//     // console.log(gotData.param2);
+// }
+// exports.onNavigatingTo = onNavigatingTo;
+var frameModule =require("ui/frame");
+function onNavigatingTo(args) {
+    var page = args.object;
+    page.bindingContext = createViewModel();
 }
 exports.onNavigatingTo = onNavigatingTo;
-
 
 var Observable = require("data/observable").Observable;
 var ObservableArray = require("data/observable-array").ObservableArray;
 
-var page;
-var items = new ObservableArray([]);
 var pageData = new Observable();
 
-exports.pageLoaded = function(args) {
-    page = args.object;
+ function pageLoaded(args) {
+    var page = args.object;
+    var items = new ObservableArray([]);
     page.bindingContext = pageData;
 
     items.push(
         {
             itemName: "Dolor de Cabeza",
-            itemDesc: "Migraña, Estres, Tensión",
+            itemDesc: "Síntomas: Migraña, Estres, Tensión",
             itemImage: "~/images/headache.jpg"
+
         },
+
         {
             itemName: "Dolor Estomacal",
-            itemDesc: "Nauseas, Estreñimiento, Diarrea, Gases...",
+            itemDesc: "Síntomas: Nauseas, Estreñimiento, Diarrea, Gases...",
             itemImage: "~/images/stomache.jpg"
+           
         },
+
+        {
+            itemName: "Dolor Muscular",
+            itemDesc: "Síntomas: Artritis, Espasmo, Dolor de Espalda",
+            itemImage: "~/images/artritis.jpg"
+
+        }, 
+
         {
             itemName: "Alergias",
-            itemDesc: "Picor en la nariz, Estornudos, Picor en el cuerpo...",
+            itemDesc: "Síntomas: Picor en la nariz, Estornudos, Picor en el cuerpo...",
             itemImage: "~/images/allergies.jpg"
+            
         }
-    )
 
+    )
      pageData.set("items", items);
 
 };
+exports.pageLoaded = pageLoaded;
 
-
+exports.cabezaPage=function() {
+    // console.log("Navigating");
+    var navigationOptions={
+        moduleName:'pages/infoPage/pages/cabezaPage/cabezaPage'//,
+        // context:{param1: "value1",
+        //         param2: "value2"
+        //         }
+    }
+    
+    frameModule.topmost().navigate(navigationOptions);
+}
